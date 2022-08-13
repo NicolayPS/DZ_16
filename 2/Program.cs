755523dece165afb8dad46d7f72f0100ec7bfeef
oksanaPC2 <77opod@gmail.com>
2022-08-13 15:51:53 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxProduct = products[0];
+            foreach(Product e in products)
+            {
+                if (e.Price > maxProduct.Price)
+                {
+                    maxProduct = e;
+                }
+            }
+            Console.WriteLine($"Номер продукта- {maxProduct.Code} \nНазвание продукта- {maxProduct.Name} \nЦена продукта- {maxProduct.Price} р/кг");
+            Console.ReadKey();
+        }
+    }
+}
